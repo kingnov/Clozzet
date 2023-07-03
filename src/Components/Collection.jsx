5bed573508cafa2f890e25ebc0f3fabaffdc9d65
muliro <mmuliro59@gmail.com>
2023-07-03 17:08:50 +0300

    made changes on collections component

--- src/Components/Collection.jsx
+++ src/Components/Collection.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-           
-export default function Collection({casual, collection,shop, image}){
-    return(
-        <div className="container">
-            
-            <div>
-               <p>{casual}</p>
-                <h4>{collection}</h4>
-                <p>{shop}</p>
-                <img src={image} alt="" />
-            </div>
-            
-         </div>
-    )
+import { AiFillCaretRight } from "react-icons/ai";
+export default function Collection({ casual, collection, image }) {
+  return (
+    <div className="container">
+      <div>
+        <p>{casual}</p>
+        <h4>{collection}</h4>
+        <p className="flex"><span>Shop Now</span>  <span><AiFillCaretRight/> </span></p>
+        <img src={image} alt="" width={150} />
+      </div>
+    </div>
+  );
 }
--- src/Components/Collections.jsx
+++ src/Components/Collections.jsx
@@ -1,18 +1,42 @@
 import React from "react";
-import MensCollection from "../assets/images/mens.png"
-import LuxuryWatches from "../assets/images/watches.png"   
-import HandBag from "../assets/images/hbags.png"      
-import ladiesCollection from "../assets/images/ladies.png"   
+import MensCollection from "../assets/images/mens.png";
+import LuxuryWatches from "../assets/images/watches.png";
+import HandBag from "../assets/images/hbags.png";
+import ladiesCollection from "../assets/images/ladies.png";
 import Collection from "./Collection";
 
-export default function Collections(){
-    return(
-        <div className="container flex mt-4 grid-cols-2 grid-rows-2">
-            < Collection casual="Casual Collection" collection="Mens Collecton" shop="Shop Now" image={MensCollection}/>
-            <Collection casual="Casual Collection" collection="Mens Collecton" shop="Shop Now" image={LuxuryWatches}/>
-            <Collection casual="Casual Collection" collection="Mens Collecton" shop="Shop Now" image={HandBag}/>
-            <Collection casual="Casual Collection" collection="Mens Collecton" shop="Shop Now" image={ladiesCollection}/>
-        </div>  
-
-    )
+export default function Collections() {
+  return (
+    <div className="container mx-auto grid grid-cols-2 grid-rows-3">
+      <div className="bg-[#F0EFEF] row-span-2">
+        <Collection
+          casual="Casual Collection"
+          collection="Mens Collecton" 
+          image={MensCollection}
+        />
+      </div>
+      <div className="bg-[#E3F9FB] row-span-1">
+        <Collection
+          casual="Casual Collection"
+          collection="Luxury Watches"
+          image={LuxuryWatches}
+        />
+      </div>
+     
+      <div className="bg-[#FEDBDB] row-span-2">
+        <Collection
+          casual="Casual Collection"
+          collection="Ladies Collecton"
+          image={ladiesCollection}
+        />
+      </div>
+      <div className="bg-[#FFF1F1] row-span-1">
+        <Collection
+          casual="Casual Collection"
+          collection="Handbags"
+          image={HandBag}
+        />
+      </div>
+    </div>
+  );
 }
